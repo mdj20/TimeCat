7ad91e515d5c751f88256a620413a9b48c55fa8d
mdj20 <mdj20@pitt.edu>
2015-06-18 17:55:24 -0400

    1 Junit test added

--- app/src/main/java/cs1530/timecat/TimerInfoInputActivity.java
+++ app/src/main/java/cs1530/timecat/TimerInfoInputActivity.java
@@ -4,15 +4,32 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.NumberPicker;
 
 import cs1530.timecat.R;
 
 public class TimerInfoInputActivity extends ActionBarActivity {
 
+    NumberPicker hourPicker;
+    NumberPicker minutePicker;
+    NumberPicker secondPicker;
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer_info_input);
+
+        setNumberPickerValues();
+
+
+
+
+
+
     }
 
     @Override
@@ -36,4 +53,25 @@ public class TimerInfoInputActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    // initalize numberpickers and sets min max value parameters
+    private void setNumberPickerValues(){
+
+        // initialize with Application environment
+        hourPicker = new NumberPicker(getApplication());
+        minutePicker = new NumberPicker(getApplication());
+        secondPicker = new NumberPicker(getApplication());
+
+        setMinMax(hourPicker,0,100);
+        setMinMax(minutePicker,0,59);
+        setMinMax(secondPicker,0,59);
+
+    }
+
+    private void setMinMax(NumberPicker np, int min ,int max){
+        np.setMinValue(min);
+        np.setMaxValue(max);
+    }
+
 }
