9794e1383f86ad35e6bffa1723040c6d367f5ad9
mdj20 <mdj20@pitt.edu>
2015-06-23 06:15:29 -0400

    added a test file

---
+++ app/src/androidTest/java/cs1530/timecat/TimeStepInfoTest.java
@@ -0,0 +1,72 @@
+package cs1530.timecat;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by matthew on 6/23/15.
+ */
+public class TimeStepInfoTest extends TestCase {
+
+    public void testSetNotes() throws Exception {
+
+    }
+
+    public void testGetNotes() throws Exception {
+
+    }
+
+    public void testSetTitle() throws Exception {
+
+    }
+
+    public void testGetTitle() throws Exception {
+
+    }
+
+    public void testSetDuration() throws Exception {
+
+    }
+
+    public void testGetDuration() throws Exception {
+
+    }
+
+    public void testSetPriority() throws Exception {
+
+        TimeStepInfo tsi = new TimeStepInfo(0,0,0,"None","none");
+
+
+        for(int i = 0 ; i < 100 ; i++){
+            tsi.setPriority(i);
+            assertEquals(tsi.getPriority(),i);
+        }
+
+    }
+
+    public void testGetPriority() throws Exception {
+
+        TimeStepInfo tsi = new TimeStepInfo(0,0,0,"None","none");
+
+        for(int i = 0 ; i < 100 ; i++){
+            tsi.setPriority(i);
+            assertEquals(tsi.getPriority(),i);
+        }
+
+
+    }
+
+    public void testCompareTo() throws Exception {
+
+
+
+
+    }
+
+    public void testDescribeContents() throws Exception {
+
+    }
+
+    public void testWriteToParcel() throws Exception {
+
+    }
+}
