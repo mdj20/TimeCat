14330605530c06812e5b5e0dbbd8f3cf38a3ca3d
mdj20 <mdj20@pitt.edu>
2015-06-23 14:53:36 -0400

    update 6/22 alarm working

--- app/src/androidTest/java/cs1530/timecat/TimeStepInfoTest.java
+++ app/src/androidTest/java/cs1530/timecat/TimeStepInfoTest.java
@@ -9,25 +9,126 @@ public class TimeStepInfoTest extends TestCase {
 
     public void testSetNotes() throws Exception {
 
+        StringBuilder testString = new StringBuilder("ABC Test Title");
+
+        TimeStepInfo tsi = new TimeStepInfo(0,0,0,testString.toString(),"NONE");
+
+        for (int i = 0 ; i < 100 ; i++){
+
+            testString.delete(0,testString.length());
+
+            testString.append(i);
+
+            tsi.setTitle(testString.toString());
+
+            assertTrue(testString.toString().equals(tsi.getTitle()));
+
+        }
+
+
+
     }
 
     public void testGetNotes() throws Exception {
+
+        StringBuilder testString = new StringBuilder("ABC Test Title");
+
+        TimeStepInfo tsi = new TimeStepInfo(0,0,0,testString.toString(),"NONE");
+
+        for (int i = 0 ; i < 100 ; i++){
+
+            testString.delete(0,testString.length());
+
+            testString.append(i);
+
+            tsi.setTitle(testString.toString());
+
+            assertTrue(testString.toString().equals(tsi.getTitle()));
+
+        }
+
+
+
+
 
     }
 
     public void testSetTitle() throws Exception {
 
+
+
+
+        StringBuilder testString = new StringBuilder("ABC Test Title");
+
+        TimeStepInfo tsi = new TimeStepInfo(0,0,0,testString.toString(),"NONE");
+
+        for (int i = 0 ; i < 100 ; i++){
+
+            testString.delete(0,testString.length());
+
+            testString.append(i);
+
+            tsi.setTitle(testString.toString());
+
+            assertTrue(testString.toString().equals(tsi.getTitle()));
+
+        }
+
+
+
+
+
+
     }
 
     public void testGetTitle() throws Exception {
 
+        StringBuilder testString = new StringBuilder("ABC Test Title");
+
+        TimeStepInfo tsi = new TimeStepInfo(0,0,0,testString.toString(),"NONE");
+
+        for (int i = 0 ; i < 100 ; i++){
+
+            testString.delete(0,testString.length());
+
+            testString.append(i);
+
+            tsi.setTitle(testString.toString());
+
+            assertTrue(testString.toString().equals(tsi.getTitle()));
+
+        }
+
+
+
+
+
+
+
     }
 
     public void testSetDuration() throws Exception {
+        TimeStepInfo tsi = new TimeStepInfo(0,0,0,"None","none");
+
+
+        for(int i = 0 ; i < 10000 ; i++){
+            tsi.setDuration(i);
+            assertEquals(tsi.getDuration(),i);
+        }
 
     }
 
     public void testGetDuration() throws Exception {
+
+        TimeStepInfo tsi = new TimeStepInfo(0,0,0,"None","none");
+
+
+        for(int i = 0 ; i < 10000 ; i++){
+            tsi.setDuration(i);
+            assertEquals(tsi.getDuration(),i);
+        }
+
+
 
     }
 
